66079930e727d5ef0057d925abd5f1932b291026
Jeovan Lindo <jeovanjrlindo@gmail.com>
2022-10-28 07:21:55 -0300

    1.4

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const btnmobile = window.document.querySelector("#btn-mobile")
 
-btnmobile.addEventListener('click', togglemenu)
+const btnMobile = window.document.querySelector ('#btn-mobile')
 
-function togglemenu(){
-    const nav = window.document.querySelector("nav")
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault() 
+    const nav = window.document.querySelector ('nav')
     nav.classList.toggle('active')
+    const active = nav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active)
+    
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+    }
 }
